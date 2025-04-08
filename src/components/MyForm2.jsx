30d91213b86ec93c38497e7efa0fa0cbb538c9a2
Dinesh107 <dineshjr107@gmail.com>
2025-04-08 11:57:09 +0530

    fourth commit

--- src/components/MyForm2.jsx
+++ src/components/MyForm2.jsx
@@ -4,7 +4,7 @@ function MyForm2() {
     //  const [name, setName] = useState("");
     //  const [age, setAge] = useState("");
     //  const [email, setEmail] = useState("");
-    const [inputs, setInputs] = useState({});
+    const [inputs, setInputs] = useState({email: "@gmail.com", age: 10, about: "I am a student" });
         // console.log("current state: ", name);
         function handleSubmit(e) {
             e.preventDefault(); // prevent page reload when form is submitted
@@ -28,12 +28,29 @@ function MyForm2() {
         <br /><br />
         <label htmlFor="">
           Enter Your Age:
-          <input type="text" name="age" onChange={handleChange} />
+          <input type="text" name="age" onChange={handleChange} value={inputs.age}/>
         </label>
         <br /><br />
         <label htmlFor="">
           Enter Your Email:
-          <input type="text" name="email" onChange={handleChange} />
+          <input type="text" name="email" onChange={handleChange} value={inputs.email}/>
+        </label>
+        <br /><br />
+        <label htmlFor="">
+          Enter Your Country:
+         <select name="country" onChange={handleChange} value={inputs.country} >
+          <option value="">Select</option>
+          <option value="usa">USA</option>
+          <option value="india" >India</option>
+          <option value="china">China</option>
+          <option value="japan">Japan</option>
+          <option value="australia">Australia</option>
+         </select>
+        </label>
+        <br /><br />
+        <label htmlFor="">
+          Enter About you:
+          <textarea name="about" value={inputs.about} onChange={handleChange} ></textarea>
         </label>
         <br /><br />
         <input type="submit" value="submit form" />
